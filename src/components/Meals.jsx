56eb2e3b41127d7a36a215bf99cb662de1c0671f
Ayo <Ayo.meshe@gmail.com>
2023-12-01 14:52:19 +0000

    Adding useHttp custom hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,21 +1,16 @@
-import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import useHttp from "./Hooks/useHttp";
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", {}, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-
-      if (!response.ok) {
-        // ..
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
 
   return (
     <ul id="meals">
